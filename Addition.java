e4cfd5da93c249eae16a30ed3ee0bfe5e8e9e160
adityasinghsuryawanshi <adityasinghsuryawanshi66@gmail.com>
2024-10-24 01:41:18 +0530

    Added the new file of Addition

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+
+
+        int a = 19;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
